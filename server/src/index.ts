71d06ccc0133c3ef0a25ffdd2b27e40893518fb9
Kibetkipsang <denniskipsang2000@gmail.com>
2026-08-10 14:41:44 +0300

    build

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://chama.keptwise.com",
+    origin: "https://beta.greysilicon.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
